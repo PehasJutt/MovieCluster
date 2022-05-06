e08c1ce444f01a7f57f03e93f93c989821f1b457
Hassan Ahmad Sarfraz <hassans@manaky.com>
2022-05-06 17:58:17 +0500

    Uploading the project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using EAD_Project_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EAD_Project_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+       /* public string Index()
+        {
+            return "AOA from Index Action Method";
+        }
+
+        public string Index_02()
+        {
+            return "AOA from Index_02 Action Method";
+        }*/
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EAD_Project_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Login(string username, string password)
+        {
+            return View("Views/Home/Index.cshtml");
+        }
+    }
+}
